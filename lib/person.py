2fb19f3fb6f5e41e396bbc5b1fd64eb1f83eac7f
kevin mayaka <kevinabuga3@gmail.com>
2024-05-27 14:58:53 +0300

    initial commit

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+
+    def walk(walk):
+        print("The person is walking.")
 
     #Instance method definition
-    pass
+mayaka = Person()
+mayaka.walk()
+
+mayaka.talk()
+   
